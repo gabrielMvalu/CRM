10d8cee91acc4295514c83e91812dbf8feaef0cf
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-21 15:37:29 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -1,14 +1,20 @@
 import streamlit as st
-
+from PIL import Image
+import os
 
 # Sidebar for uploading and displaying logo and text
-st.sidebar.title("CRM predictii ors. Bailesti")
+st.sidebar.title("CRM Predictii Oras Bailesti")
+
 logo_path = "LogoSTR.PNG"
-try:
-    logo = Image.open(logo_path)
-    st.sidebar.image(logo, use_column_width=True)
-except IOError:
-    st.sidebar.error("Eroare la încărcarea logo-ului.")
+
+# Check if the file exists
+if not os.path.exists(logo_path):
+    st.sidebar.error("Logo file not found: " + logo_path)
+else:
+    try:
+        logo = Image.open(logo_path)
+        st.sidebar.image(logo, use_column_width=True)
+    except IOError as e:
+        st.sidebar.error("Error opening logo file: " + str(e))
+
     st.sidebar.markdown("<small>© Castemill S.R.L.</small>", unsafe_allow_html=True)
-
-
