cdce178b0b4f2876916e1a9a89b704c011fae2f8
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 19:53:34 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -8,41 +8,7 @@ import pydeck as pdk
 col1, col2 = st.columns([1, 1])
 
 with col1:
-   chart_data = pd.DataFrame(
-      np.random.randn(10, 2) / [50, 50] + [44.02, 23.34],
-      columns=['lat', 'lon'])
-   
-   st.pydeck_chart(pdk.Deck(
-       map_style=None,
-       initial_view_state=pdk.ViewState(
-           latitude=44.02,
-           longitude=23.34,
-           zoom=11,
-           pitch=50,
-       ),
-       layers=[
-           pdk.Layer(
-              'HexagonLayer',
-              data=chart_data,
-              get_position='[lon, lat]',
-              radius=200,
-              elevation_scale=4,
-              elevation_range=[0, 1000],
-              pickable=True,
-              extruded=True,
-           ),
-           pdk.Layer(
-               'ScatterplotLayer',
-               data=chart_data,
-               get_position='[lon, lat]',
-               get_color='[200, 30, 0, 160]',
-               get_radius=200,
-           ),
-       ],
-   ))
-
-with col2:
-   chart_data = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+   chart_data = pd.DataFrame(np.random.randn(30, 3), columns=["a", "b", "c"])
    
    st.vega_lite_chart(
       chart_data,
@@ -110,6 +76,43 @@ with col2:
       }
 
    )
+
+
+with col2:
+   chart_data = pd.DataFrame(
+      np.random.randn(10, 2) / [50, 50] + [44.02, 23.34],
+      columns=['lat', 'lon'])
+   
+   st.pydeck_chart(pdk.Deck(
+       map_style=None,
+       initial_view_state=pdk.ViewState(
+           latitude=44.02,
+           longitude=23.34,
+           zoom=11,
+           pitch=50,
+       ),
+       layers=[
+           pdk.Layer(
+              'HexagonLayer',
+              data=chart_data,
+              get_position='[lon, lat]',
+              radius=200,
+              elevation_scale=4,
+              elevation_range=[0, 1000],
+              pickable=True,
+              extruded=True,
+           ),
+           pdk.Layer(
+               'ScatterplotLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               get_color='[200, 30, 0, 160]',
+               get_radius=200,
+           ),
+       ],
+   ))
+
+
 
 
 def read_docx(file):
