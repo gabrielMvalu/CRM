4fbad09644695102ae8a59fbd4d6532bfae52bf9
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 19:35:09 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -5,54 +5,56 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 
-chart_data = pd.DataFrame(
-   np.random.randn(10, 2) / [50, 50] + [44.02, 23.34],
-   columns=['lat', 'lon'])
+col1, col2 = st.columns([10, 20])
+with col1:
+   chart_data = pd.DataFrame(
+      np.random.randn(10, 2) / [50, 50] + [44.02, 23.34],
+      columns=['lat', 'lon'])
+   
+   st.pydeck_chart(pdk.Deck(
+       map_style=None,
+       initial_view_state=pdk.ViewState(
+           latitude=44.02,
+           longitude=23.34,
+           zoom=11,
+           pitch=50,
+       ),
+       layers=[
+           pdk.Layer(
+              'HexagonLayer',
+              data=chart_data,
+              get_position='[lon, lat]',
+              radius=200,
+              elevation_scale=4,
+              elevation_range=[0, 1000],
+              pickable=True,
+              extruded=True,
+           ),
+           pdk.Layer(
+               'ScatterplotLayer',
+               data=chart_data,
+               get_position='[lon, lat]',
+               get_color='[200, 30, 0, 160]',
+               get_radius=200,
+           ),
+       ],
+   ))
 
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=44.02,
-        longitude=23.34,
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
-        pdk.Layer(
-           'HexagonLayer',
-           data=chart_data,
-           get_position='[lon, lat]',
-           radius=200,
-           elevation_scale=4,
-           elevation_range=[0, 1000],
-           pickable=True,
-           extruded=True,
-        ),
-        pdk.Layer(
-            'ScatterplotLayer',
-            data=chart_data,
-            get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
-            get_radius=200,
-        ),
-    ],
-))
-
-
-chart_data = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
-
-st.vega_lite_chart(
-   chart_data,
-   {
-       "mark": {"type": "circle", "tooltip": True},
-       "encoding": {
-           "x": {"field": "a", "type": "quantitative"},
-           "y": {"field": "b", "type": "quantitative"},
-           "size": {"field": "c", "type": "quantitative"},
-           "color": {"field": "c", "type": "quantitative"},
-       },
-   },
-)
+with col2:
+   chart_data = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+   
+   st.vega_lite_chart(
+      chart_data,
+      {
+          "mark": {"type": "circle", "tooltip": True},
+          "encoding": {
+              "x": {"field": "a", "type": "quantitative"},
+              "y": {"field": "b", "type": "quantitative"},
+              "size": {"field": "c", "type": "quantitative"},
+              "color": {"field": "c", "type": "quantitative"},
+          },
+      },
+   )
 
 
 
