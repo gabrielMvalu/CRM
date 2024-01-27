a122808784d73c0787d42141da4115b251f51b3e
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 19:13:21 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -3,12 +3,40 @@ import streamlit as st
 from docx import Document
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
-df = pd.DataFrame(
-    np.random.randn(10, 20) / [50, 50] + [44.02, 23.34],
-    columns=['lat', 'lon'])
+chart_data = pd.DataFrame(
+   np.random.randn(100, 2) / [50, 50] + [44.02, 23.34],
+   columns=['lat', 'lon'])
 
-st.map(df)
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=44.02,
+        longitude=23.34,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
 
 
 def read_docx(file):
