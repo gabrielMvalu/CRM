6f84aba2ecbce7f8790ab11c669f547599b8fd21
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 13:47:25 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -4,7 +4,8 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from docx import Documnet
+from docx import Document
+
 
 
 # Sidebar for uploading and displaying logo and text
