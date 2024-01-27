8fcde03ba1b9992ce07b180b536e02e7f46e8dba
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 13:41:58 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -22,6 +22,22 @@ else:
 
 st.sidebar.markdown("<small>© Castemill S.R.L.</small>", unsafe_allow_html=True)
 
+
+def docx_to_df(doc):
+    tables = []
+    for table in doc.tables:
+        df = [['' for _ in range(len(table.columns))] for _ in range(len(table.rows))]
+        for i, row in enumerate(table.rows):
+            for j, cell in enumerate(row.cells):
+                if cell.text:
+                    df[i][j] = cell.text
+        tables.append(pd.DataFrame(df))
+    return tables
+
+
+uploaded_file = st.sidebar.file_uploader("Alege un fișier .docx", type="docx")
+
+
 df = pd.DataFrame(
     [
        {"command": "st.selectbox", "rating": 4, "is_widget": True},
@@ -44,3 +60,17 @@ st.bar_chart(
     chart_data.set_index("Variatia precipitatii per locatie"),
     use_container_width=True
 )
+
+
+if uploaded_file is not None:
+    doc = Document(uploaded_file)
+    tables = docx_to_df(doc)
+    if tables:
+        for i, table in enumerate(tables):
+            st.write(f"Tabelul {i+1}")
+            st.dataframe(table)
+    else:
+        st.write("Documentul nu conține tabele.")
+else:
+    st.write("Vă rugăm să încărcați un document .docx în meniul din stânga.")
+
