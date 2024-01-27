19f365ed77e10ba2667da4dd9e2cd70953ee9ec5
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-27 13:43:43 +0200

    Update arm_app.py

--- arm_app.py
+++ arm_app.py
@@ -22,19 +22,6 @@ else:
 
 st.sidebar.markdown("<small>© Castemill S.R.L.</small>", unsafe_allow_html=True)
 
-
-def docx_to_df(doc):
-    tables = []
-    for table in doc.tables:
-        df = [['' for _ in range(len(table.columns))] for _ in range(len(table.rows))]
-        for i, row in enumerate(table.rows):
-            for j, cell in enumerate(row.cells):
-                if cell.text:
-                    df[i][j] = cell.text
-        tables.append(pd.DataFrame(df))
-    return tables
-
-
 uploaded_file = st.sidebar.file_uploader("Alege un fișier .docx", type="docx")
 
 
@@ -61,16 +48,26 @@ st.bar_chart(
     use_container_width=True
 )
 
+def docx_to_df(doc):
+    tables = []
+    for table in doc.tables:
+        df = [['' for _ in range(len(table.columns))] for _ in range(len(table.rows))]
+        for i, row in enumerate(table.rows):
+            for j, cell in enumerate(row.cells):
+                if cell.text:
+                    df[i][j] = cell.text
+        tables.append(pd.DataFrame(df))
+    return tables
 
-if uploaded_file is not None:
-    doc = Document(uploaded_file)
-    tables = docx_to_df(doc)
-    if tables:
-        for i, table in enumerate(tables):
-            st.write(f"Tabelul {i+1}")
-            st.dataframe(table)
+    if uploaded_file is not None:
+        doc = Document(uploaded_file)
+        tables = docx_to_df(doc)
+        if tables:
+            for i, table in enumerate(tables):
+                st.write(f"Tabelul {i+1}")
+                st.dataframe(table)
+        else:
+            st.write("Documentul nu conține tabele.")
     else:
-        st.write("Documentul nu conține tabele.")
-else:
-    st.write("Vă rugăm să încărcați un document .docx în meniul din stânga.")
+        st.write("Vă rugăm să încărcați un document .docx în meniul din stânga.")
 
